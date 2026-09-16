e384f645ee34914cff5c118fe3fe5cc17df83272
Shaily <shaily@oz-digital.com.au>
2026-09-16 10:44:32 +0530

    project data updated

--- src/data/ProjectData.ts
+++ src/data/ProjectData.ts
@@ -30,6 +30,49 @@ import project_img_29 from "@/assets/images/karalee/1.webp";
 import project_img_30 from "@/assets/images/karalee/2.webp";
 import project_img_31 from "@/assets/images/karalee/3.webp";
 import project_img_32 from "@/assets/images/karalee/4.webp";
+import project_img_33 from "@/assets/images/kenmore/1.webp";
+import project_img_34 from "@/assets/images/kenmore/2.webp";
+import project_img_35 from "@/assets/images/kenmore/3.webp";
+import project_img_36 from "@/assets/images/kenmore/4.webp";
+import project_img_37 from "@/assets/images/loganvillage/1.webp";
+import project_img_38 from "@/assets/images/loganvillage/2.webp";
+import project_img_39 from "@/assets/images/loganvillage/3.webp";
+import project_img_40 from "@/assets/images/loganvillage/4.webp";
+import project_img_41 from "@/assets/images/mountwarrenpark/1.webp";
+import project_img_42 from "@/assets/images/mountwarrenpark/2.webp";
+import project_img_43 from "@/assets/images/mountwarrenpark/3.webp";
+import project_img_44 from "@/assets/images/morayfield/1.webp";
+import project_img_45 from "@/assets/images/morayfield/2.webp";
+import project_img_46 from "@/assets/images/morayfield/3.webp";
+import project_img_47 from "@/assets/images/munruben/1.webp";
+import project_img_48 from "@/assets/images/munruben/2.webp";
+import project_img_49 from "@/assets/images/munruben/3.webp";
+import project_img_50 from "@/assets/images/narangba/1.webp";
+import project_img_51 from "@/assets/images/narangba/2.webp";
+import project_img_52 from "@/assets/images/narangba/3.webp";
+import project_img_53 from "@/assets/images/redbankplains/1.webp";
+import project_img_54 from "@/assets/images/redbankplains/2.webp";
+import project_img_55 from "@/assets/images/redbankplains/3.webp";
+import project_img_56 from "@/assets/images/redbankplains/4.webp";
+import project_img_57 from "@/assets/images/rochedalesouth/1.webp";
+import project_img_58 from "@/assets/images/rochedalesouth/2.webp";
+import project_img_59 from "@/assets/images/shailerpark/1.webp";
+import project_img_60 from "@/assets/images/shailerpark/2.webp";
+import project_img_61 from "@/assets/images/sunnybank/1.webp";
+import project_img_62 from "@/assets/images/sunnybank/2.webp";
+import project_img_63 from "@/assets/images/sunnybank/3.webp";
+import project_img_64 from "@/assets/images/thornlands/1.webp";
+import project_img_65 from "@/assets/images/thornlands/2.webp";
+import project_img_66 from "@/assets/images/thornlands/3.webp";
+import project_img_67 from "@/assets/images/thornlands/4.webp";
+import project_img_68 from "@/assets/images/toowong/1.webp";
+import project_img_69 from "@/assets/images/toowong/2.webp";
+import project_img_70 from "@/assets/images/toowong/3.webp";
+import project_img_71 from "@/assets/images/toowoomba/1.webp";
+import project_img_72 from "@/assets/images/toowoomba/2.webp";
+import project_img_73 from "@/assets/images/toowoomba/3.webp";
+import project_img_74 from "@/assets/images/toowoomba/4.webp";
+
 
 
 import { StaticImageData } from "next/image";
@@ -148,7 +191,153 @@ const project_data: DataType[] = [
     name: "11.88kW System Installed in Karalee",
     title: "11.88kW System Installed in Karalee",
   },
+  {
+    id: 11,
+    images: [
+      project_img_33,
+      project_img_34,
+	  project_img_35,
+	  project_img_36,
+    ],
+    name: "13.3kW System installed in Kenmore",
+    title: "13.3kW System installed in Kenmore",
+  },
+  
+  {
+    id: 12,
+    images: [
+      project_img_37,
+      project_img_38,
+	  project_img_39,
+	  project_img_40,
+    ],
+    name: "13.2kW System Installed in Logan Village",
+    title: "13.2kW System Installed in Logan Village",
+  },
 
+  {
+    id: 13,
+    images: [
+      project_img_41,
+      project_img_42,
+	  project_img_43,
+    ],
+    name: "10.56kW System Installed in Mount Warren Park",
+    title: "10.56kW System Installed in Mount Warren Park",
+  },
+
+  
+
+  {
+    id: 14,
+    images: [
+      project_img_44,
+      project_img_45,
+	  project_img_46,
+    ],
+    name: "6.6kW System Installed in Morayfield",
+    title: "6.6kW System Installed in Morayfield",
+  },
+
+  {
+    id: 15,
+    images: [
+      project_img_47,
+      project_img_48,
+	  project_img_49,
+    ],
+    name: "13.2kW System Installed in Munruben",
+    title: "13.2kW System Installed in Munruben",
+  },
+
+  
+
+  {
+    id: 16,
+    images: [
+      project_img_50,
+      project_img_51,
+	  project_img_52,
+    ],
+    name: "6.6kW System Installed in Narangba",
+    title: "6.6kW System Installed in Narangba",
+  },
+{
+    id: 17,
+    images: [
+      project_img_53,
+      project_img_54,
+	  project_img_55,
+	  project_img_56,
+    ],
+    name: "13.2kW System Installed in Redbank Plains",
+    title: "13.2kW System Installed in Redbank Plains",
+  },
+{
+    id: 18,
+    images: [
+      project_img_57,
+      project_img_58,
+    ],
+    name: "6.6kW System Installed in Rochedale South",
+    title: "6.6kW System Installed in Rochedale South",
+  },
+  {
+    id: 19,
+    images: [
+      project_img_59,
+      project_img_60,
+    ],
+    name: "5kW System Installed in Shailer Park",
+    title: "5kW System Installed in Shailer Park",
+  },
+  {
+    id: 20,
+    images: [
+      project_img_61,
+      project_img_62,
+      project_img_63,
+    ],
+    name: "6.6kW System installed in Sunnybank",
+    title: "6.6kW System installed in Sunnybank",
+  },
+
+  
+  {
+    id: 21,
+    images: [
+      project_img_64,
+      project_img_65,
+      project_img_66,
+	  project_img_67,
+    ],
+    name: "6.6kW System installed in Thornlands",
+    title: "6.6kW System installed in Thornlands",
+  },
+
+   
+  {
+    id: 22,
+    images: [
+      project_img_68,
+      project_img_69,
+      project_img_70,
+    ],
+    name: "13.2kW System Installed In Toowong",
+    title: "13.2kW System Installed In Toowong",
+  },
+
+ {
+    id: 23,
+    images: [
+      project_img_71,
+      project_img_72,
+      project_img_73,
+	  project_img_74,
+    ],
+    name: "6.6kW System Installed in Toowoomba",
+    title: "6.6kW System Installed in Toowoomba",
+  },
 
   
   
